38081379ec2fbe4711f044b337f0e6823f8b6c41
Lalit Bisht <lbisht1996@gmail.com>
2023-01-14 21:08:18 +0530

    added some more changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import LoginPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-// import Header from "./components/Header";
-import { useState } from "react";
+import Header from "./components/Header";
+import { useCallback, useState } from "react";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  /*  function handleLogin() {
-    setIsLoggedIn(!isLoggedIn);
-  } */
-  return (
-    <div className="app">
-      {/* <Header isLoggedIn={isLoggedIn} handleLogin={handleLogin} /> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </div>
-  );
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	let handleLogin = useCallback(() => {
+		setIsLoggedIn(!isLoggedIn);
+	}, []);
+	return (
+		<div className="app">
+			<Header isLoggedIn={isLoggedIn} handleLogin={handleLogin} />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
