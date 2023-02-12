20f8e9466d9587bf71424a9f042061bff5c6b78c
Lalit Bisht <lbisht1996@gmail.com>
2023-02-12 18:16:41 +0530

    clean sweep

--- src/interfaces/HeaderInterface.ts
+++
@@ -1,6 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-interface HeaderInterface {
-    isLoggedIn: boolean,
-    handleLogin: Dispatch<SetStateAction<boolean>>
-}
-export default HeaderInterface;
